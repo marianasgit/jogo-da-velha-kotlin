d12506a509ba9968a040c00de4df2ab0c65a858c
mariana <marianabicla@gmIL.COM>
2022-03-24 16:49:36 -0300

    desenhando telas sem o backend

---
+++ app/src/main/java/com/example/jogodavelha/EscolhaDeAdversario.kt
@@ -0,0 +1,11 @@
+package com.example.jogodavelha
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EscolhaDeAdversario : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_escolha_de_adversario)
+    }
+}
