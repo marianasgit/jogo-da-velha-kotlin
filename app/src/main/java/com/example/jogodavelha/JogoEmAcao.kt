b7ea536570cbd69b52d612519e4eccc749f4905d
mariana <marianabicla@gmIL.COM>
2022-03-24 16:52:40 -0300

    adicionando activity principal

---
+++ app/src/main/java/com/example/jogodavelha/JogoEmAcao.kt
@@ -0,0 +1,11 @@
+package com.example.jogodavelha
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class JogoEmAcao : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_jogo_em_acao)
+    }
+}
