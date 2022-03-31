7b3b4626b423c4a9eccc915fc55a9663e128f0b3
mariana <marianabicla@gmIL.COM>
2022-03-31 16:55:42 -0300

    finalizando layouts

--- app/src/main/java/com/example/jogodavelha/EscolhaDeAdversario.kt
+++ app/src/main/java/com/example/jogodavelha/EscolhaDeAdversario.kt
@@ -1,11 +1,27 @@
 package com.example.jogodavelha
 
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class EscolhaDeAdversario : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_escolha_de_adversario)
+
+        val button: Button = findViewById(R.id.jogoAmigo)
+
+        button.setOnClickListener {
+            button.background = getDrawable(R.drawable.botao_escuro)
+
+            fun menuJogarComAmigo() {
+
+                val intent = Intent(this, JogandoComAmigo::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
